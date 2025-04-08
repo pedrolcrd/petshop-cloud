05e6f7876acb92f36236ceae68780a7ff573f56b
pedrolcrd <150477043+pedrolcrd@users.noreply.github.com>
2025-04-08 01:14:49 -0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,15 @@ output "app_service_url" {
   value       = "https://${azurerm_linux_web_app.app.default_hostname}"
   description = "URL do App Service"
 }
+
+output "suffix" {
+  value = random_id.suffix.hex
+}
+
+output "webapp_name" {
+  value = azurerm_linux_web_app.app.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
